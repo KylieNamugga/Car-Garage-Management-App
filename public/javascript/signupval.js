2bcfb56b60a7da0acff357cff7e037aa7637578a
KylieNamugga <96869130+KylieNamugga@users.noreply.github.com>
2022-06-10 16:52:13 +0300

    new validation file

--- public/javascript/signupval.js
+++ public/javascript/signupval.js
@@ -27,64 +27,65 @@ const validation = () => {
             fNameError.innerHTML = "";
             firstname.style.border = "";
         }
+    }
 
-        // lastname validation
-        if (lastname.value == "") {
-            lNameError.innerHTML = "Last name is required";
+    // lastname validation
+    if (lastname.value == "") {
+        lNameError.innerHTML = "Last name is required";
+        lastname.style.border = "1px solid red";
+        lastname.focus();
+        return false;
+    } else {
+        const lnameRegex = /^[A-Z][a-z]$/;
+        if (lnameRegex.test(lastname.value) === false) {
+            lNameError.innerHTML = "Last name should start with a capital letter";
             lastname.style.border = "1px solid red";
             lastname.focus();
             return false;
         } else {
-            const lnameRegex = /^[A-Z][a-z]$/;
-            if (lnameRegex.test(lastname.value) === false) {
-                lNameError.innerHTML = "Last name should start with a capital letter";
-                lastname.style.border = "1px solid red";
-                lastname.focus();
-                return false;
-            } else {
-                lNameError.innerHTML = "";
-                lastname.style.border = "";
-            }
+            lNameError.innerHTML = "";
+            lastname.style.border = "";
+        }
+    }
 
-            // email validation
-            if (email.value == "") {
-                emailError.innerHTML = "Email is required";
-                email.style.border = "1px solid red";
-                email.focus();
-                return false;
-            } else {
-                const emailRegex = /^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/;
-                if (emailRegex.test(email.value) === false) {
-                    emailError.innerHTML = "Invalid email address";
-                    email.style.border = "1px solid red";
-                    email.focus();
-                    return false;
-                } else {
-                    emailError.innerHTML = "";
-                    email.style.border = "";
-                }
+    // email validation
+    if (email.value == "") {
+        emailError.innerHTML = "Email is required";
+        email.style.border = "1px solid red";
+        email.focus();
+        return false;
+    } else {
+        const emailRegex = /^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/;
+        if (emailRegex.test(email.value) === false) {
+            emailError.innerHTML = "Invalid email address";
+            email.style.border = "1px solid red";
+            email.focus();
+            return false;
+        } else {
+            emailError.innerHTML = "";
+            email.style.border = "";
+        }
+    }
 
-                // password validation
-                if (password.value == "") {
-                    passwordError.innerHTML = "Password is required";
-                    password.style.border = "1px solid red";
-                    password.focus();
-                    return false;
-                } else {
-                    const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z]){8,}$/;
-                    if (passwordRegex.test(password.value) === false) {
-                        passwordError.innerHTML = "Password should contain at least 8 characters, one uppercase and one lowercase letter";
-                        password.style.border = "1px solid red";
-                        password.focus();
-                        return false;
-                    } else {
-                        passwordError.innerHTML = "";
-                        password.style.border = "";
-                    }
-                }
-            }
+    // password validation
+    if (password.value == "") {
+        passwordError.innerHTML = "Password is required";
+        password.style.border = "1px solid red";
+        password.focus();
+        return false;
+    } else {
+        const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z]){8,}$/;
+        if (passwordRegex.test(password.value) === false) {
+            passwordError.innerHTML = "Password should contain at least 8 characters, one uppercase and one lowercase letter";
+            password.style.border = "1px solid red";
+            password.focus();
+            return false;
+        } else {
+            passwordError.innerHTML = "";
+            password.style.border = "";
         }
     }
 }
 
-return validation();
+
+
